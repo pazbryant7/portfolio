f6c55826de7bdd0be7350e59768d9513db6c15d7
Bryant Paz <plbryant@hotmail.com>
2023-12-04 12:00:33 -0600

    feat: finishing variety section

--- src/components/Variety.jsx
+++ src/components/Variety.jsx
@@ -7,27 +7,31 @@ const Variety = () => {
                 <div className="tokyo_tm_title">
                     <div className="title_flex">
                         <div className="left">
-                            <span>Drafts</span>
-                            <h3>Latest drafts</h3>
+                            <span>Personal space</span>
+                            <h3>Latest updates</h3>
                         </div>
                     </div>
                 </div>
                 <ul>
                     <li>
                         <div className="list_inner">
-                            <div className="image">
+                            <a
+                                href="https://github.com/plbryant/dotfiles"
+                                className="image"
+                                target="_blank"
+                            >
                                 <img
-                                    src="assets/img/thumbs/react.png"
+                                    src="assets/img/thumbs/terminal.png"
                                     alt="thumb"
                                 />
                                 <div
                                     className="main"
                                     style={{
                                         backgroundImage:
-                                            'url(assets/img/thumbs/react.png)',
+                                            'url(assets/img/thumbs/terminal.png)',
                                     }}
                                 ></div>
-                            </div>
+                            </a>
                             <div className="details">
                                 <div className="extra">
                                     <p className="date">
@@ -35,7 +39,42 @@ const Variety = () => {
                                         <span>20 December 2022</span>
                                     </p>
                                 </div>
-                                <h3 className="title">My personal dotfiles</h3>
+                                <h3 className="title">Personal dotfiles</h3>
+                                <div className="tokyo_tm_read_more">
+                                    <a>
+                                        <span>Read More</span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="list_inner">
+                            <a
+                                className="image"
+                                href="https://github.com/plbryant/nvim"
+                                target="_blank"
+                            >
+                                <img
+                                    src="assets/img/thumbs/nvim.png"
+                                    alt="thumb"
+                                />
+                                <div
+                                    className="main"
+                                    style={{
+                                        backgroundImage:
+                                            'url(assets/img/thumbs/nvim.png)',
+                                    }}
+                                ></div>
+                            </a>
+                            <div className="details">
+                                <div className="extra">
+                                    <p className="date">
+                                        By <a href="#">Bryant Paz</a>
+                                        <span>4 December 2023</span>
+                                    </p>
+                                </div>
+                                <h3 className="title">Neovim configuration</h3>
                                 <div className="tokyo_tm_read_more">
                                     <a>
                                         <span>Read More</span>
