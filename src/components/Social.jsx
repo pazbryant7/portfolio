abdb5a0847d65cd2bd8144eff2f3430a8df0e2d8
Bryant Paz <pazbryant@proton.me>
2025-01-13 14:19:48 -0600

    chore: update default instagram user name

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -4,7 +4,7 @@ const SocialShare = [
     { iconName: 'twitter', link: 'https://twitter.com/bryantpaz7' },
     {
         iconName: 'instagram',
-        link: 'https://www.instagram.com/pazbryant7/',
+        link: 'https://www.instagram.com/bryantpaz7/',
     },
     {
         iconName: 'github',
